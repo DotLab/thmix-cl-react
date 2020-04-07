31b0312928fba9f8d989a86892223508e706fc27
Kailang <kailangfu@gmail.com>
2020-04-07 10:52:35 -0500

    Fix badge display

--- src/components/gradeBadges.js
+++ src/components/gradeBadges.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const GradeBadgeS = ({grade}) => <span className="Bgc($purple)! badge badge-pill">{grade}</span>;
+export const GradeBadgeS = ({grade}) => <span className="Bgc($purple)! badge badge-dark badge-pill">{grade}</span>;
 export const GradeBadgeA = ({grade}) => <span className="badge badge-warning badge-pill">{grade}</span>;
 export const GradeBadgeB = ({grade}) => <span className="badge badge-light badge-pill">{grade}</span>;
 export const GradeBadgeC = ({grade}) => <span className="badge badge-info badge-pill">{grade}</span>;
@@ -9,11 +9,11 @@ export const GradeBadgeF = ({grade}) => <span className="badge badge-dark badge-
 
 export const GradeBadge = ({gradeLevel, grade}) => {
   switch (gradeLevel) {
-    case 'S': return <GradeBadgeS grade={grade}/>;
-    case 'A': return <GradeBadgeA grade={grade}/>;
-    case 'B': return <GradeBadgeB grade={grade}/>;
-    case 'C': return <GradeBadgeC grade={grade}/>;
-    case 'D': return <GradeBadgeD grade={grade}/>;
-    default: return <GradeBadgeF grade={grade}/>;
+    case 'S': return <GradeBadgeS grade={grade || gradeLevel}/>;
+    case 'A': return <GradeBadgeA grade={grade || gradeLevel}/>;
+    case 'B': return <GradeBadgeB grade={grade || gradeLevel}/>;
+    case 'C': return <GradeBadgeC grade={grade || gradeLevel}/>;
+    case 'D': return <GradeBadgeD grade={grade || gradeLevel}/>;
+    default: return <GradeBadgeF grade={grade || gradeLevel}/>;
   }
 };
