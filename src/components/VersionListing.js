54a5d179dc26bbc2b3a8c654e10714762e8092d9
hinataizayoi <hinataizayoi@gmail.com>
2020-04-15 02:36:46 -0500

    Add require login to download

--- src/components/VersionListing.js
+++ src/components/VersionListing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import queryString from 'query-string';
 import ReactMarkdown from 'react-markdown';
 import {formatDate} from '../utils';
@@ -12,12 +13,13 @@ class VersionRow extends React.Component {
         <div className="col-md-3 pb-3">
           <div className="badge badge-pill badge-secondary">{formatDate(p.date)}</div>
           <p>{p.name}</p>
-          <a className="btn btn-outline-secondary btn-sm" href={p.url}><i class="fas fa-cloud-download-alt"></i> Download</a>
+          {p.isLoggedIn ? <a className="btn btn-outline-secondary btn-sm" href={p.url}><i class="fas fa-cloud-download-alt"></i> Download</a> :
+          <Link className="btn btn-outline-secondary btn-sm" to="/login"><i class="fas fa-cloud-download-alt"></i> Download</Link>}
         </div>
         <div className="col-md-9">
-          {p.path.split('.').pop() === 'ipa' && <h4><i class="fab fa-apple"></i> {p.version} ({p.build})</h4>}
-          {p.path.split('.').pop() === 'apk' && <h4><i class="fab fa-android"></i> {p.version} ({p.build})</h4>}
-          {(p.path.split('.').pop() !== 'apk' && p.path.split('.').pop() !== 'ipa') && <h4><i class="fas fa-question"></i> {p.version} ({p.build})</h4>}
+          {p.path.split('.').pop() === 'ipa' ? <h4><i class="fab fa-fw fa-apple"></i> {p.version} ({p.build})</h4> :
+            p.path.split('.').pop() === 'apk' ? <h4><i class="fab fa-fw fa-android"></i> {p.version} ({p.build})</h4> :
+            <h4><i class="fas fa-fw fa-question"></i> {p.version} ({p.build})</h4>}
 
           <ReactMarkdown skipHtml={true} source={p.desc}/>
         </div>
@@ -46,15 +48,16 @@ export default class VersionListing extends React.Component {
 
   render() {
     const s = this.state;
+    const isLoggedIn = this.app.state.user ? true : false;
 
     return <div className="container-fluid">
       <section className="Bgc($gray-700) P(30px) text-light shadow">
         <h2 className="row Fw(n)">Versions</h2>
       </section>
-      <section className="mt-2 mb-3 shadow border">
-        <div className="Bgc($gray-100)">
+      <section className="mt-2 mb-3">
+        <div className="">
           <div className="row">
-            {s.versions.map((version) => <VersionRow {...version} key={version.id}/>)}
+            {s.versions.map((version) => <VersionRow {...version} key={version.id} isLoggedIn={isLoggedIn}/>)}
           </div>
         </div>
       </section>
