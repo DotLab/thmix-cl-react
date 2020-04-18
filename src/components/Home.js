50d10df7ef8b25cb4cb01a4151e9eba524654cd4
Kailang <kailangfu@gmail.com>
2020-04-18 04:01:20 -0500

    Add cover image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomeShowcase from './HomeShowcase.jpg';
+import 星月夜 from '../c7肘/星月夜.jpg';
 
 import {rpc} from '../apiService';
 import {Translation as Tr} from '../translationService';
@@ -28,11 +28,12 @@ class App extends React.Component {
     return <section className="container Pb(100px)">
       <div
         className="Bgp(c) Bgz(cv) px-4 py-3 text-light text-center text-lg-right shadow"
-        style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, .8)), url(${HomeShowcase})`}}
+        style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, .6)), url(${星月夜})`}}
       >
         <div><small>{s.revision}</small></div>
         <div><small>{s.playerCount} <Tr src="players"/>, {s.onlineCount} <Tr src="currently online"/>, {s.gameCount} <Tr src="games"/></small></div>
-        <h2 className="Mt(100px) h4 font-weight-normal font-italic mb-0">Touhou Mix: <Tr src="A Touhou Project Music Game"/></h2>
+        <div><small>illustration by <a className="C(white)" href="https://www.pixiv.net/en/artworks/34844544">c7肘</a></small></div>
+        <h2 className="Mt(150px) h4 font-weight-normal font-italic mb-0">Touhou Mix: <Tr src="A Touhou Project Music Game"/></h2>
         <small className="C($pink)"><Tr src="Perform Touhou Project music on your device!"/></small>
         <div className="mt-3">
           <a className="btn btn-primary ml-2" href="https://itunes.apple.com/us/app/touhou-mix-a-touhou-game/id1454875483"><Tr src="App Store"/> <i className="fab fa-app-store"></i></a>
@@ -45,7 +46,7 @@ class App extends React.Component {
         <div className="mt-2">
           <a className="btn btn-info ml-2" href="https://discord.gg/m2BeMbj"><Tr src="Join Discord"/> <i className="fab fa-discord"></i></a>
         </div>
-        <div className="Mt(100px) small text-center"><Tr src="peak online players"/> {s.peakOnlineCount}</div>
+        <div className="Mt(150px) small text-center"><Tr src="peak online players"/> {s.peakOnlineCount}</div>
       </div>
     </section>;
   }
